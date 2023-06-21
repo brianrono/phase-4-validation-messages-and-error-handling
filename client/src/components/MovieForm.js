a7aa06f291f08128505c7671213773a7f4cea402
brianrono <brianrono16@gmail.com>
2023-06-21 21:58:09 +0300

    To solve error messages

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -12,7 +13,15 @@ function MovieForm() {
     category: "",
     discount: false,
     female_director: false,
-  });
+  }
+  .then((response) => {
+    if (response.ok) {
+      response.json().then((newMovie) => console.log(newMovie));
+    }else {
+      response.json().then((errorData) => setErrors(errorData.errors));
+    }
+  })
+  );
 
   function handleSubmit(e) {
     e.preventDefault();
